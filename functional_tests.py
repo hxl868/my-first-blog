ce18aed22c748597cd3e67b351546a217339daac
Harry Liddall <hxl868@student.bham.ac.uk>
2020-09-05 20:58:14 +0100

    testing

--- functional_tests.py
+++ functional_tests.py
@@ -13,32 +13,38 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_edit(self):
         self.browser.get('http://localhost:8000')
+        #user clicks on cv link
         link = self.browser.find_element_by_link_text('CV')
         link.click()
+        #
         time.sleep(15)
-        python_button = self.browser.find_elements_by_xpath("//a[@class='btn btn-default pull-right' and @href = '/cv/edit/']")[0]
-        python_button.click()
+        #user clicks on edit button
+        edit_button = self.browser.find_elements_by_xpath("//a[@class='btn btn-default pull-right' and @href = '/cv/edit/']")[0]
+        edit_button.click()
         time.sleep(2)
+        #user selects text area labeld 'other'
         textarea = self.browser.find_element_by_xpath("//p//textarea[@name='other']")
         textarea.click()
+        #store original text so can it can be restored after test
         tmp = textarea.get_attribute('value')
         print(tmp)
         textarea.clear()
-
+        #user inputs their text and saves it
         textarea.send_keys("test")
 
         save_button = self.browser.find_element_by_xpath("//form//button[@type='submit']")
         save_button.click()
 
+        #on the view cv page the 'other' field should now appear as 'Other: test'
+        #in the list of elements in the cv 'other' field is index 9
         elements = self.browser.find_elements(By.TAG_NAME, 'p')
         for i in elements:
         	print(i.text)
         self.assertEquals(elements[9].text, 'Other: test')
-        #testtext= self.browser.find_element_by_xpath("//p//b[text()='Other:']")
-        ##self.assertIn('test', testtext)
-
-        python_button = self.browser.find_elements_by_xpath("//a[@class='btn btn-default pull-right' and @href = '/cv/edit/']")[0]
-        python_button.click()
+        
+        #reutrn other field to original value
+        edit_button = self.browser.find_elements_by_xpath("//a[@class='btn btn-default pull-right' and @href = '/cv/edit/']")[0]
+        edit_button.click()
         time.sleep(2)
         textarea = self.browser.find_element_by_xpath("//p//textarea[@name='other']")
         textarea.click()
@@ -47,9 +53,6 @@ class NewVisitorTest(unittest.TestCase):
 
         save_button = self.browser.find_element_by_xpath("//form//button[@type='submit']")
         save_button.click()
-
-
-        time.sleep(10)
         
 
     def test_can_view_cv_page(self):  
@@ -67,7 +70,7 @@ class NewVisitorTest(unittest.TestCase):
         elements = self.browser.find_elements(By.TAG_NAME, 'b')
 
         #all elements of the cv should be shown on the page.
-        #elements = self.browser.find_elements(By.TAG_NAME, 'p'
+        
         onpage = []
         expected = ['Name:','Address:','Nationality:','Phonenumber:','Email:','Statement:','Employment history:','Academic achievements:','Interests:','Other:','References:']
         for e in elements:
