69c1ea0d2a65b10b18c5b3bb2b18b9904d168cf5
Harry Liddall <hxl868@student.bham.ac.uk>
2020-09-05 20:48:39 +0100

    testing

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):  
 
@@ -9,6 +10,47 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):  
         self.browser.quit()
+
+    def test_can_edit(self):
+        self.browser.get('http://localhost:8000')
+        link = self.browser.find_element_by_link_text('CV')
+        link.click()
+        time.sleep(15)
+        python_button = self.browser.find_elements_by_xpath("//a[@class='btn btn-default pull-right' and @href = '/cv/edit/']")[0]
+        python_button.click()
+        time.sleep(2)
+        textarea = self.browser.find_element_by_xpath("//p//textarea[@name='other']")
+        textarea.click()
+        tmp = textarea.get_attribute('value')
+        print(tmp)
+        textarea.clear()
+
+        textarea.send_keys("test")
+
+        save_button = self.browser.find_element_by_xpath("//form//button[@type='submit']")
+        save_button.click()
+
+        elements = self.browser.find_elements(By.TAG_NAME, 'p')
+        for i in elements:
+        	print(i.text)
+        self.assertEquals(elements[9].text, 'Other: test')
+        #testtext= self.browser.find_element_by_xpath("//p//b[text()='Other:']")
+        ##self.assertIn('test', testtext)
+
+        python_button = self.browser.find_elements_by_xpath("//a[@class='btn btn-default pull-right' and @href = '/cv/edit/']")[0]
+        python_button.click()
+        time.sleep(2)
+        textarea = self.browser.find_element_by_xpath("//p//textarea[@name='other']")
+        textarea.click()
+        textarea.clear()
+        textarea.send_keys(tmp)
+
+        save_button = self.browser.find_element_by_xpath("//form//button[@type='submit']")
+        save_button.click()
+
+
+        time.sleep(10)
+        
 
     def test_can_view_cv_page(self):  
         
